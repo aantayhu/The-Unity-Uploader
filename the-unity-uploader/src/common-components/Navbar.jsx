d76c08074ae1eb61c0f6c6b2c4ccaeb16e0d8077
Angel Antayhua-Reynoso <114554667+aantayhu@users.noreply.github.com>
2025-03-30 10:28:12 -0400

    updated navbar
    navbar final changes

--- the-unity-uploader/src/common-components/Navbar.jsx
+++ the-unity-uploader/src/common-components/Navbar.jsx
@@ -1,18 +1,7 @@
-import {useState, useEffect} from 'react';
-import {Link, useLocation} from 'react-router-dom';
+import {Link} from 'react-router-dom';
 import './Navbar.css';
 
 function Navbar() {
-
-    const [selectedID, changeSelectedID] = useState('games');
-    const location = useLocation();
-
-    useEffect(() => {
-        const path = location.pathname.split('/')[1]; // Get the first part of the path after '/'
-        console.log(path || 'games');
-        changeSelectedID(path || 'games'); // If no path, default to 'games'
-      }, [location]); // Update whenever location changes
-
 
     const changeWordSize = (givenID, newSize) => {
         const elem = document.getElementById(givenID);
@@ -27,38 +16,30 @@ function Navbar() {
             <div id="logo"><strong>The Unity Uploader</strong></div>
             <div className='navbar-container'>
                 <Link to='/'>
-                    <h3 id='games' 
-                    className={selectedID === 'games' ? 'selected' : ''} // Apply 'selected' class if the ID matches
-                    onMouseEnter={() => changeWordSize('games', 40)} 
-                        onMouseLeave={() => changeWordSize('games', 18.75)}
-                        onClick={() => changeSelectedID('games')}>
+                    <h3 id='games' onMouseEnter={() => changeWordSize('games', 40)} 
+                        onMouseLeave={() => changeWordSize('games', 18.75)}>
                             Games
                     </h3>
                 </Link>
 
-                <Link to='/main-using-unity'>
-                    <h3 id='using-unity' onMouseEnter={() => changeWordSize('using-unity', 40)}
-                    className={selectedID === 'main-using-unity' ? 'selected' : ''} // Apply 'selected' class if the ID matches 
-                        onMouseLeave={() => changeWordSize('using-unity', 18.75)}
-                        onClick={() => changeSelectedID('using-unity')}>
+                <Link to='/'>
+                    <h3 id='using-unity' onMouseEnter={() => changeWordSize('using-unity', 40)} 
+                        onMouseLeave={() => changeWordSize('using-unity', 18.75)}>
                             Using Unity
                     </h3>
                 </Link>
 
+
                 <Link to='/upload'>
-                    <h3 id='upload' onMouseEnter={() => changeWordSize('upload', 40)} 
-                    className={selectedID === 'upload' ? 'selected' : ''} // Apply 'selected' class if the ID matches
-                        onMouseLeave={() => changeWordSize('upload', 18.75)}
-                        onClick={() => changeSelectedID('upload')}>
+                    <h3 id='pload'u onMouseEnter={() => changeWordSize('upload', 40)} 
+                        onMouseLeave={() => changeWordSize('upload', 18.75)}>
                             Upload
                     </h3>
                 </Link>
 
                 <Link to='/about'>
                     <h3 id='about' onMouseEnter={() => changeWordSize('about', 40)} 
-                    className={selectedID === 'about' ? 'selected' : ''} // Apply 'selected' class if the ID matches
-                        onMouseLeave={() => changeWordSize('about', 18.75)}
-                        onClick={() => changeSelectedID('about')}>
+                        onMouseLeave={() => changeWordSize('about', 18.75)}>
                             About
                     </h3>
                 </Link>
