dba6bf15ca205e95c753b037161d66213347fe9f
dburnaye <dburnaye@gmu.edu>
2025-03-30 01:32:01 -0400

    Game page related changes

--- the-unity-uploader/src/common-components/Navbar.jsx
+++ the-unity-uploader/src/common-components/Navbar.jsx
@@ -1,14 +1,35 @@
+
 import './Navbar.css'
 
 function Navbar() {
 
+    const changeWordSize = (givenID, newSize) => {
+        const elem = document.getElementById(givenID);
+        if (elem != null) {
+            elem.style.fontSize = `${newSize}px`;
+            elem.style.cursor = (newSize == 40) ? 'pointer' : 'default';
+        }
+    }
+
     return(
         <>
             <div className='navbar-container'>
-                <h3>Games</h3>
-                <h3>Using Unity</h3>
-                <h3>Upload</h3>
-                <h3>About</h3>
+                <h3 id='games' onMouseEnter={() => changeWordSize('games', 40)} 
+                    onMouseLeave={() => changeWordSize('games', 18.75)}>
+                        Games
+                </h3>
+                <h3 id='using-unity' onMouseEnter={() => changeWordSize('using-unity', 40)} 
+                    onMouseLeave={() => changeWordSize('using-unity', 18.75)}>
+                        Using Unity
+                </h3>
+                <h3 id='upload' onMouseEnter={() => changeWordSize('upload', 40)} 
+                    onMouseLeave={() => changeWordSize('upload', 18.75)}>
+                        Upload
+                </h3>
+                <h3 id='about' onMouseEnter={() => changeWordSize('about', 40)} 
+                    onMouseLeave={() => changeWordSize('about', 18.75)}>
+                        About
+                </h3>
             </div>
         </>
     )
--- the-unity-uploader/src/webpages/GamePage.jsx
+++ the-unity-uploader/src/webpages/GamePage.jsx
@@ -1,5 +1,27 @@
+import Navbar from '../common-components/Navbar';
+import GameObject from '../common-components/GameObject';
+import Image1 from '../photos/1713801616273.jpg';
+import Image2 from '../photos/1740444203614.jpg';
+import './GamePage.css';
+
 function GamePage() {
-    
+    return (
+        <>
+            <div id="logo"><strong>The Unity Uploader</strong></div>
+            <Navbar />
+            <h1>All The Games:</h1>
+            <div id='game-container'>
+                <GameObject imageURL={Image1} 
+                            title='Example 1' 
+                            description='This is the first exmaple of a game object'/>
+
+                <GameObject imageURL={Image2}
+                            title='Example 2' 
+                            description='This is the second exmaple of a game object'/>
+            </div>
+            <button id='upload-button'>Upload your game!</button>
+        </>
+    );
 }
 
 export default GamePage;
