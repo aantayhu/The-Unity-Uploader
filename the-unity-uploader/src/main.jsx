33a86ec61edd1b4133d6a3198552de193c6c37b1
543e-crypto <shevynbuhary@gmail.com>
2025-03-30 02:50:19 -0400

    final commit
    final commit

--- the-unity-uploader/src/main.jsx
+++ the-unity-uploader/src/main.jsx
@@ -3,17 +3,23 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import GamePage from './webpages/GamePage'
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
 import PlatformerPage from './webpages/using-unity/PlatformerPage'
 import PuzzlePage from './webpages/using-unity/PuzzlePage'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <PuzzlePage />
 =======
+=======
+>>>>>>> Stashed changes
 import UploadPage from './webpages/UploadPage';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <UploadPage />
+<<<<<<< Updated upstream
+>>>>>>> Stashed changes
+=======
 >>>>>>> Stashed changes
   </StrictMode>,
 )
