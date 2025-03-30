ca9fa364a0cc180c3cda446dc1c3ca14e903876b
raid0819 <ryanle0819@gmail.com>
2025-03-29 22:59:07 -0400

    PlatformerPage not done

--- the-unity-uploader/src/main.jsx
+++ the-unity-uploader/src/main.jsx
@@ -3,9 +3,10 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import GamePage from './webpages/GamePage'
 import AboutPage from './webpages/AboutPage'
+import PlatformerPage from './webpages/using-unity/PlatformerPage'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <AboutPage />
+    <PlatformerPage />
   </StrictMode>,
 )
--- the-unity-uploader/src/webpages/using-unity/PlatformerPage.jsx
+++ the-unity-uploader/src/webpages/using-unity/PlatformerPage.jsx
@@ -1,5 +1,39 @@
 function PlatformerPage() {
-    
+    return(
+        <>
+            <div id = "all">
+                <h1>
+                    Platformer
+                </h1>
+                <p>
+                    What's a platformer? - A platformer is a genre of games where the player moves around the level to get all the way to the end.
+                </p>
+                <p>
+                    Methods related to movement
+                    
+                    {/* <pre>
+                        <code>
+                            {`
+                            
+                                private Rigidbody2D rb;
+                                // Start is called before the first frame update
+                                void Start()
+                                {
+                                    rb = this.gameObject.getComponent<Rigidbody2D>();   
+                                }
+
+                                void FixedUpdate()
+                                {
+                                    float x_movement = Input.GetAxis("Horizontal");
+                                    if(rb.)
+                                }
+                            `}
+                        </code> 
+                    </pre> */}
+                </p>
+            </div>
+        </>
+    )
 }
 
 export default PlatformerPage;
