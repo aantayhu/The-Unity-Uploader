3782643b7fd40ceed1fc49dccee32d89d756f595
543e-crypto <shevynbuhary@gmail.com>
2025-03-29 23:01:31 -0400

    Upload Page
    Upload page so far

--- the-unity-uploader/src/main.jsx
+++ the-unity-uploader/src/main.jsx
@@ -2,11 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import GamePage from './webpages/GamePage'
-import AboutPage from './webpages/AboutPage'
-import PlatformerPage from './webpages/using-unity/PlatformerPage'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <PlatformerPage />
+    <GamePage />
   </StrictMode>,
 )
