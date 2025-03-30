f603dfada362380ff33a590555052ac80bdbb0f8
raid0819 <ryanle0819@gmail.com>
2025-03-29 22:11:14 -0400

    AboutPage done

--- the-unity-uploader/src/main.jsx
+++ the-unity-uploader/src/main.jsx
@@ -2,9 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import GamePage from './webpages/GamePage'
+import AboutPage from './webpages/AboutPage'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <GamePage />
+    <AboutPage />
   </StrictMode>,
 )
