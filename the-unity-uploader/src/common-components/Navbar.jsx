659755ed5e9115451621d296e052e0032c00b271
dburnaye <dburnaye@gmu.edu>
2025-03-30 10:41:57 -0400

    hopeful correction of navbar

--- the-unity-uploader/src/common-components/Navbar.jsx
+++ the-unity-uploader/src/common-components/Navbar.jsx
@@ -1,51 +1,71 @@
-import {Link} from 'react-router-dom';
+import { useState } from 'react';
+import { Link, useLocation } from 'react-router-dom';
 import './Navbar.css';
 
 function Navbar() {
+  const [selectedID, changeSelectedID] = useState('games');
+  const location = useLocation(); // Hook to get the current location
 
-    const changeWordSize = (givenID, newSize) => {
-        const elem = document.getElementById(givenID);
-        if (elem != null) {
-            elem.style.fontSize = `${newSize}px`;
-            elem.style.cursor = (newSize == 40) ? 'pointer' : 'default';
-        }
-    }
+  // Effectively update selectedID based on current location/path
+  useEffect(() => {
+    const path = location.pathname.split('/')[1]; // Get the first part of the path after '/'
+    changeSelectedID(path || 'games'); // If no path, default to 'games'
+  }, [location]); // Update whenever location changes
 
-    return(
-        <>
-            <div id="logo"><strong>The Unity Uploader</strong></div>
-            <div className='navbar-container'>
-                <Link to='/'>
-                    <h3 id='games' onMouseEnter={() => changeWordSize('games', 40)} 
-                        onMouseLeave={() => changeWordSize('games', 18.75)}>
-                            Games
-                    </h3>
-                </Link>
+  return (
+    <>
+      <div id="logo"><strong>The Unity Uploader</strong></div>
+      <div className='navbar-container'>
+        <Link to='/'>
+          <h3 
+            id='games' 
+            className={selectedID === 'games' ? 'selected' : ''}
+            onMouseEnter={() => changeWordSize('games', 40)} 
+            onMouseLeave={() => changeWordSize('games', 18.75)}
+            onClick={() => changeSelectedID('games')}
+          >
+            Games
+          </h3>
+        </Link>
 
-                <Link to='/'>
-                    <h3 id='using-unity' onMouseEnter={() => changeWordSize('using-unity', 40)} 
-                        onMouseLeave={() => changeWordSize('using-unity', 18.75)}>
-                            Using Unity
-                    </h3>
-                </Link>
+        <Link to='/main-using-unity'>
+          <h3 
+            id='using-unity' 
+            className={selectedID === 'using-unity' ? 'selected' : ''}
+            onMouseEnter={() => changeWordSize('using-unity', 40)} 
+            onMouseLeave={() => changeWordSize('using-unity', 18.75)}
+            onClick={() => changeSelectedID('using-unity')}
+          >
+            Using Unity
+          </h3>
+        </Link>
 
+        <Link to='/upload'>
+          <h3 
+            id='upload' 
+            className={selectedID === 'upload' ? 'selected' : ''}
+            onMouseEnter={() => changeWordSize('upload', 40)} 
+            onMouseLeave={() => changeWordSize('upload', 18.75)}
+            onClick={() => changeSelectedID('upload')}
+          >
+            Upload
+          </h3>
+        </Link>
 
-                <Link to='/upload'>
-                    <h3 id='pload'u onMouseEnter={() => changeWordSize('upload', 40)} 
-                        onMouseLeave={() => changeWordSize('upload', 18.75)}>
-                            Upload
-                    </h3>
-                </Link>
-
-                <Link to='/about'>
-                    <h3 id='about' onMouseEnter={() => changeWordSize('about', 40)} 
-                        onMouseLeave={() => changeWordSize('about', 18.75)}>
-                            About
-                    </h3>
-                </Link>
-            </div>
-        </>
-    )
+        <Link to='/about'>
+          <h3 
+            id='about' 
+            className={selectedID === 'about' ? 'selected' : ''}
+            onMouseEnter={() => changeWordSize('about', 40)} 
+            onMouseLeave={() => changeWordSize('about', 18.75)}
+            onClick={() => changeSelectedID('about')}
+          >
+            About
+          </h3>
+        </Link>
+      </div>
+    </>
+  )
 }
 
 export default Navbar;
